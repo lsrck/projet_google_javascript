763ef983c6f197edece9b5abd7878b461556472e
Louis Aurick <louisaurick@ntymail.com>
2018-01-11 18:41:14 +0100

    popup

--- google.js
+++ google.js
@@ -1,19 +1,16 @@
-document.querySelector("button").onclick = function(apparition) 
+document.querySelector("button").onclick = function() 
 {
 document.getElementById("cacher").style.display = "block";
 document.querySelector("button").style.display = "none";
 }
 
-
-
-
-document.getElementById("boutonrecherche").onclick = function(recherchegoogle) 
+document.getElementById("boutonrecherche").onclick = function() 
 {
 	let laRecherche = document.getElementById("recherche").value;
 	window.location = "https://www.google.fr/search?q=" + laRecherche;
 }
 
-
-
-
-
+function popup () {
+window.alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}
+setTimeout(popup, 2000)
