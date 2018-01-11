62fde0de8d4b2e3c40393716c0d0a7d88f5cbb33
Louis Aurick <louisaurick@ntymail.com>
2018-01-11 18:13:58 +0100

    Ajout recherche

--- google.js
+++ google.js
@@ -1,7 +1,19 @@
-
-
-document.querySelector("button").onclick = function() {
+document.querySelector("button").onclick = function(apparition) 
+{
 document.getElementById("cacher").style.display = "block";
 document.querySelector("button").style.display = "none";
 }
+
+
+
+
+document.getElementById("boutonrecherche").onclick = function(recherchegoogle) 
+{
+	let laRecherche = document.getElementById("recherche").value;
+	window.location = "https://www.google.fr/search?q=" + laRecherche;
+}
+
+
+
+
 
