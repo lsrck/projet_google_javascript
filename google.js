c78149305f667297d28f65372621a762f768e015
Louis Aurick <louisaurick@ntymail.com>
2018-01-11 16:22:52 +0100

    Ajout bouton

---
+++ google.js
@@ -0,0 +1,7 @@
+
+
+document.querySelector("button").onclick = function() {
+document.getElementById("cacher").style.display = "block";
+document.querySelector("button").style.display = "none";
+}
+
